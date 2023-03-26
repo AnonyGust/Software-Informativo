b2953e1b9dd7efe12b45f2f9932b56d534fa1028
unknown <gustavo.lima1307@gmail.com>
2023-03-26 17:23:28 -0300

    terceiro commit

--- script.js
+++ script.js
@@ -4,60 +4,69 @@ const registerLink = document.querySelector('.register-link');
 const btnPopup = document.querySelector('.btnLogin-popup');
 const iconClose = document.querySelector('.icon-close');
 const btnRegistroPopup = document.querySelector('.btnRegistro-popup');
+const forgot = document.querySelector('.forgot-password-link');
+const backtoLogin = document.querySelector('.back-to-login');
 const response = document.getElementById("response")
 const base_uri = 'http://localhost:5230/api'
 
 registerLink.addEventListener('click', () => {
-  wrapper.classList.add('active')
+  wrapper.classList.toggle('active');
 });
 
 loginLink.addEventListener('click', () => {
-  wrapper.classList.remove('active')
+  wrapper.classList.toggle('active');
 });
 
-
 btnPopup.addEventListener('click', () => {
-  if (wrapper.classList.contains('active-popup')) {
-    wrapper.classList.remove('active-popup');
-    wrapper.classList.remove('active')
-    wrapper.classList.remove('activetwo')
-  } else {
-    wrapper.classList.add('active-popup');
-    
-  }
+  wrapper.classList.toggle('active-popup');
 });
 
 btnRegistroPopup.addEventListener('click', () => {
-  if (wrapper.classList.contains('active-popup')) {
-    wrapper.classList.remove('active-popup');
-    wrapper.classList.remove('active')
-    wrapper.classList.remove('activetwo')
-
-  } else {
-    wrapper.classList.add('active-popup');
-    wrapper.classList.add('active');
-  }
+  wrapper.classList.toggle('active-popup');
+  wrapper.classList.toggle('active');
 });
 
 iconClose.addEventListener('click', () => {
-  wrapper.classList.remove('active-popup')
+  wrapper.classList.remove('active-popup');
 });
 
-//formulário de esqueceu senha
-
-const forgot = document.querySelector('.forgot-password-link');
-const backtoLogin = document.querySelector('.back-to-login');
-
 forgot.addEventListener('click', () => {
-  wrapper.classList.add('activetwo')
+  wrapper.classList.toggle('activetwo');
 });
 
 backtoLogin.addEventListener('click', () => {
-  wrapper.classList.remove('activetwo')
+  wrapper.classList.toggle('activetwo');
 });
 
 
+
 //INTEGRACAO BASICA API
+
+function onRegisterNewUser(){
+  const obj = {
+    password: document.getElementById("register_password").value,
+    name: document.getElementById("register_name").value,
+    cpf: "111.111.111-11",
+    ra: document.getElementById("register_ra").value,
+    email: document.getElementById("register_email").value,
+    type: 'Teste'
+  }
+  const header = {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(obj)
+    }
+  
+ 
+
+  fetch(`${base_uri}/Login/CreateUser`, header).then(() => {
+    response.innerHTML = "Cadastrado com sucesso"
+  }).catch((err) => console.error(`O erro foi esse ${err}`))
+}
+
 
 //FUNÇÃO PARA REGISTRO
 function onRegisterNewUser() {
@@ -91,12 +100,11 @@ function onLoginUser() {
 //FUNÇÃO PARA LOGIN
 
 function onLoginUser(event) {
-  event.preventDefault(); // evita que o formulário seja enviado
-
+  event.preventDefault(); //EVITA que o formulario envie para outra página
   const obj = {
-    email: document.getElementById("login_email").value,
+    ra: document.getElementById("login_ra").value,
     password: document.getElementById("login_password").value
-  }
+  } //Define os valores a serem enviados de acordo com o que o usuario inserir
 
   const header = {
     method: 'POST',
@@ -105,15 +113,22 @@ function onLoginUser(event) {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify(obj)
-  }
+  }//Define o cabeçalho HTTP (necessário pra fazer a requisição e necessário seguir este padrão)
 
-  fetch(`${base_uri}/Login/LoginUser`, header)
-    .then(response => {
-      if (response.ok) {
-        window.location.href = "./pagina_adm/index.html";
-      } else {
-        console.error("Erro ao fazer login");
+  //Metodo fetch para fazer a requisição para a API
+  fetch(`${base_uri}/Login/LoginUser`, header).then((res) => { //Primeiro then é se a requisição retornou sucesso
+    res.json().then((resp) => { //res.json transforma a resposta em json
+      if(resp.logged){ //se a propriedade logged for true então faça o que tiver dentro do if
+        const token = resp.bearer //pegar o token bearer da API
+        sessionStorage.setItem("token", token); //joga o token em uma sessão (assinatura de login)
+      }else{
+        document.getElementById('error_login').innerHTML = "Usuário ou senha invalido/s" //Define mensagem no html
       }
     })
-    .catch(() => console.error("Erro ao fazer login"));
+    .catch((err) => {
+      console.log(err);
+    }) //erro tecnico
+  }).catch((err) => {
+     console.log(err) //erro tecnico
+  })
 }
